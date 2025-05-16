c9eee06f16fe690acec4d2de74449d40bcebaa28
lmndang-dev <lmndang@outlook.com>
2025-05-16 03:01:27 -0400

    added athenticated users to the firestone data base under the users collection

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -10,7 +10,13 @@ import {
 } from "firebase/auth";
 
 //Firestone database library
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  snapshotEqual,
+} from "firebase/firestore";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -43,9 +49,38 @@ export const db = getFirestore();
 
 //Function to added authenticated user to users collection
 export const createUserDocumentFromAuth = async (userAuth) => {
+  //userAuth object attribute
+  // displayName: "Le Minh Nhat Dang"
+  // email: "lmndang.toronto@gmail.com"
+  // uid: "BxbFrxVl9aN3Lyju8tEVRvi2h6d2"
+
+  //Create the references of users collection in the Data base following the unique user ID
   const userDocRef = doc(db, "users", userAuth.uid);
 
-  console.log(userDocRef);
-
+  //The snapshot is getting the collection of user in the database
   const userSnapShot = await getDoc(userDocRef);
+
+  //If the user data doesn't exist
+  //Create / set the document with the data from userAuth in my collection
+  if (!userSnapShot.exists()) {
+    //Destructured object - get display name and email address from authenticated user from Google login popup
+    const { displayName, email } = userAuth;
+
+    const createAt = new Date();
+
+    //Try to add user to data base, catch the error if it failed
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  //Check if the user data exists
+
+  return userDocRef;
 };
