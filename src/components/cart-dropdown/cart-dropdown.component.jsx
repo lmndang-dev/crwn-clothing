dda87c44605c8648e296f170fcec28348b74dcb3
lmndang-dev <lmndang@outlook.com>
2025-05-18 17:15:54 -0400

    add checkout page

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,6 +1,7 @@
 import "./cart-dropdown.styles.scss";
 
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
@@ -8,6 +9,15 @@ import CartItem from "../cart-item/cart-item.component";
 
 const CartDropdown = () => {
   const { cartItems } = useContext(CartContext);
+
+  // Import useNavigate from react-router-dom at the top of your file:
+  // import { useNavigate } from "react-router-dom";
+  const navigate = useNavigate();
+
+  const goToCheckoutHandler = () => {
+    // Navigate to the checkout page
+    navigate("/checkout");
+  };
 
   return (
     <div className="cart-dropdown-container">
@@ -17,7 +27,7 @@ const CartDropdown = () => {
           <CartItem key={item.id} cartItem={item} />
         ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,12 @@
+import "./checkout.styles.scss";
+
+const Checkout = () => {
+  return (
+    <div className="checkout-container">
+      <h1>Checkout Page</h1>
+      {/* Add your checkout form or summary here */}
+    </div>
+  );
+};
+
+export default Checkout;
