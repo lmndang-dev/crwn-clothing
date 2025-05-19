6a5d981b5e2471580f3c4fce3696209b8155118c
lmndang-dev <lmndang@outlook.com>
2025-05-19 14:42:29 -0400

    set-up reducer from Redux

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 import "./index.scss";
 
@@ -9,32 +10,36 @@ import { UserProvider } from "./contexts/user.context";
 import { CategoriesProvider } from "./contexts/categories.context";
 import { CartProvider } from "./contexts/cart.context";
 
+import { store } from "./store/store";
+
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    {/* The BrowserRouter component wraps the entire application, enabling routing functionality */}
-    {/* This allows the app to use React Router for navigation and routing */}
-    {/* The BrowserRouter component is imported from the react-router-dom library */}
-    <BrowserRouter>
-      {/* The UserProvider component wraps the App component, providing the user context to all components within the app */}
-      {/* This allows any component in the app to access the user context using the useContext hook */}
-      {/* The UserProvider component is imported from the user.context file */}
-      <UserProvider>
-        {/* The ProductsProvider component wraps the App component, providing the products context to all components within the app */}
-        {/* This allows any component in the app to access the products context using the useContext hook */}
-        {/* The ProductsProvider component is imported from the products.context file */}
-        <CategoriesProvider>
-          {/* The CartContext component wraps the App component, providing the cart context to all components within the app */}
-          {/* This allows any component in the app to access the cart context using the useContext hook */}
-          {/* The CartContext component is imported from the cart.context file */}
-          <CartProvider>
-            <App />
-          </CartProvider>
-        </CategoriesProvider>
-      </UserProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      {/* The BrowserRouter component wraps the entire application, enabling routing functionality */}
+      {/* This allows the app to use React Router for navigation and routing */}
+      {/* The BrowserRouter component is imported from the react-router-dom library */}
+      <BrowserRouter>
+        {/* The UserProvider component wraps the App component, providing the user context to all components within the app */}
+        {/* This allows any component in the app to access the user context using the useContext hook */}
+        {/* The UserProvider component is imported from the user.context file */}
+        <UserProvider>
+          {/* The ProductsProvider component wraps the App component, providing the products context to all components within the app */}
+          {/* This allows any component in the app to access the products context using the useContext hook */}
+          {/* The ProductsProvider component is imported from the products.context file */}
+          <CategoriesProvider>
+            {/* The CartContext component wraps the App component, providing the cart context to all components within the app */}
+            {/* This allows any component in the app to access the cart context using the useContext hook */}
+            {/* The CartContext component is imported from the cart.context file */}
+            <CartProvider>
+              <App />
+            </CartProvider>
+          </CategoriesProvider>
+        </UserProvider>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 );
 
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -2,4 +2,6 @@ import { combineReducers } from "redux";
 
 import { useReducer } from "./user/user.reducer";
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+  user: useReducer,
+});
